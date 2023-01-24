b8daf250d7e683d6612316b500c4d89251f04d6b
Vivian Cheng <vivianchengyy@gmail.com>
2023-01-25 06:02:24 +0800

    remove route

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
 import './App.css';
-import { Box } from '@mui/material';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { StartPage } from './components/startPage';
-import { AboutMe } from './components/aboutMe';
-import { MySkill } from './components/skill';
-import { Connect } from './components/connect';
 
 function App() {
   return (
-    <Router>
-      <Box>
-        <Routes>
-          <Route path="/" element={<StartPage />} />
-          <Route path="/about" element={<AboutMe />} />
-          <Route path="/skill" element={<MySkill />} />
-          <Route path="/contact" element={<Connect />} />
-        </Routes>
-      </Box>
-    </Router>
+    <StartPage />
   );
 }
 
