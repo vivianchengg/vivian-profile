4ea290e0f066eeddc92a85a7423cf965945b6ad6
Vivian Cheng <vivianchengyy@gmail.com>
2023-01-25 00:16:57 +0800

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './App.css';
 import { Box } from '@mui/material';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { StartPage } from './components/startPage';
+import { AboutMe } from './components/aboutMe';
+import { MySkill } from './components/skill';
+import { Connect } from './components/connect';
 
 function App() {
   return (
@@ -9,7 +12,9 @@ function App() {
       <Box>
         <Routes>
           <Route path="/" element={<StartPage />} />
-          
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/skill" element={<MySkill />} />
+          <Route path="/contact" element={<Connect />} />
         </Routes>
       </Box>
     </Router>
